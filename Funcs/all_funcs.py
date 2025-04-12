cf3d62c4764978f3309d87bcd329e4bb293865c0
Richy <97134605+Richycam@users.noreply.github.com>
2025-04-12 19:19:49 +0100

    Create all_funcs.py

---
+++ Funcs/all_funcs.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from Consts import password
+from pathlib import Path
+
+
+def init_check_pass():
+    load_dotenv("env/password.env")
+    password.password_vault = os.getenv("PASS")
+    password_check = input("Vualt Password? >")
+    if password.password_vault == password_check:
+        main()
+
+    
+
+
+env_file_path = Path('env/password.env')
+# Define the content you want to write to the .env file
+
+
+def main():
+# Load variables from .env file
+    load_dotenv("env/vault.env")
+    print(os.getenv("secrets"))
+    input("VUALT>")
