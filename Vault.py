3008afcf5fcf00a5bb2f795a8658176522907d55
Richy <97134605+Richycam@users.noreply.github.com>
2025-04-17 10:17:18 +0100

    Update Vault.py

--- Vault.py
+++ Vault.py
@@ -1,8 +1,6 @@
 from Funcs import all_funcs
 
+all_funcs.init_check_pass() #initalise the password checker
 
-
-all_funcs.init_check_pass()
-
-while all_funcs.init_check_pass():
+while all_funcs.init_check_pass(): #while loop to move into main
     all_funcs.main()
